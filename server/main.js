3e69ef4fe3d74de1207e4e72d0f95ff015604be4
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-06-21 16:17:58 -0700

    Export Detections

--- server/main.js
+++ server/main.js
@@ -2,6 +2,6 @@ import '../imports/api/action.js';
 import '../imports/api/facial_expression.js';
 import '../imports/api/speech.js';
 import '../imports/api/speechbubbles.js';
-import '../imports/api/vision.js';
+export { Detections } from '../imports/api/vision.js';
 
 export { MediaFiles } from '../imports/api/media.js'
